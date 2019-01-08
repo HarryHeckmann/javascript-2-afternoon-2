29947ce0a15fcd259194f8de7cd969fb3e32cd16
Harry Heckmann <harry@Harrys-MacBook-Pro.local>
2019-01-08 14:51:14 -0600

    add updated js file

--- practice.js
+++ practice.js
@@ -17,8 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -32,8 +33,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
-
+function last(arr){
+   return arr[arr.length-1];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -47,8 +49,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
+function looper(family){
+  for (i=0; i<family.length; i++) {
+    alert(family[i]);
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -62,8 +67,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-//Code Here
+function reversedLooper(letters){
+  for (i=letters.length-1; i>-1;i--){
+    alert(letters[i]);
+  }
+}
 
+reversedLooper(letters);
 
 
 ////////// PROBLEM 5 //////////
@@ -76,9 +86,18 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Write a function named evenFinder that is given nums as it's only argument.
   Return an array that contains the even numbers from the nums array.
 */
+var evensOnly = [];
 
-//Code Here
+function evenFinder(nums){
+  evensOnly = nums.filter(function(val, i, arr){
+    return val % 2 === 0;
+  })
+return evensOnly;
+}
 
+evenFinder(nums);
+ 
+console.log(evensOnly);
 
 
 
